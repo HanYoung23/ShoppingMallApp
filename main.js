ea57a46dc853b78846c40d96f602b638c5dbe79a
HanYoung23 <67942048+HanYoung23@users.noreply.github.com>
2020-09-05 21:55:49 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 "use strict";
 
-const shirtsArray = ["img/blue_t.png", "img/pink_t.png", "img/yellow_t.png"];
+var shirtsArray = ["img/blue_t.png", "img/pink_t.png", "img/yellow_t.png"];
 
-const skirtArray = ["img/blue_s.png", "img/pink_s.png", "img/yellow_s.png"];
+var skirtArray = ["img/blue_s.png", "img/pink_s.png", "img/yellow_s.png"];
 
-const pantsArray = ["img/blue_p.png", "img/pink_p.png", "img/yellow_p.png"];
+var pantsArray = ["img/blue_p.png", "img/pink_p.png", "img/yellow_p.png"];
 
-const clothesArray = [
+var blueArray = ["img/blue_t.png", "img/blue_s.png", "img/blue_p.png"];
+
+var pinkArray = ["img/pink_t.png", "img/pink_s.png", "img/pink_p.png"];
+
+var yellowArray = ["img/yellow_t.png", "img/yellow_s.png", "img/yellow_p.png"];
+
+var clothesArray = [
   "img/blue_t.png",
   "img/pink_t.png",
   "img/yellow_t.png",
@@ -18,7 +24,7 @@ const clothesArray = [
   "img/yellow_p.png",
 ];
 
-const textsArray = [
+var textsArray = [
   "male, small size",
   "female, large size",
   "male, large size",
@@ -29,26 +35,22 @@ const textsArray = [
   "female, large size",
   "male, small size",
 ];
-
-console.log(document.getElementsByClassName("shirts_btn").onclick === true);
-
+// section 생성--------------------------------------
 const section = document.createElement("section");
 section.className = "shoppingList";
 document.body.appendChild(section);
-
-// 반복 시작 ------------
-
-function makeUl() {
+// ul + li 반복--------------------------------------
+var originalFunction = function () {
   const ul = document.createElement("ul");
   ul.className = "listUl";
   section.appendChild(ul);
-  if (textsArray.length === 9) {
+  if (textsArray.length === 9 && clothesArray.length === 9) {
     var x = 2;
   } else {
     var x = 1;
   }
   for (let i = 0; i < x; i++) {
-    for (let i = 0; i < textsArray.length; i++) {
+    for (let i = 0; i < clothesArray.length; i++) {
       const li = document.createElement("li");
       const img = document.createElement("img");
       const div = document.createElement("div");
@@ -63,5 +65,58 @@ function makeUl() {
       ul.appendChild(li);
     }
   }
+};
+originalFunction();
+
+// 버튼 클릭 이벤트 ----------------------------
+const buttonArray = [
+  ".shirts_btn",
+  ".pant_btn",
+  ".skirt_btn",
+  ".blue_btn",
+  ".pink_btn",
+  ".yellow_btn",
+];
+const buttonMenuArray = [
+  "shirtsArray",
+  "pantsArray",
+  "skirtArray",
+  "blueArray",
+  "pinkArray",
+  "yellowArray",
+];
+
+function makeButtonOnclick() {
+  for (let i = 0; i < buttonArray.length; i++) {
+    document.querySelector(buttonArray[i]).onclick = function () {
+      chooseClothes(buttonMenuArray[i]);
+    };
+  }
 }
-makeUl();
+makeButtonOnclick();
+
+function chooseClothes(btnclick) {
+  document.querySelector("ul").remove();
+  clothesArray = btnclick;
+  originalFunction();
+}
+
+/**
+document.querySelector(".shirts_btn").onclick = function btnclicked() {
+  chooseClothes(shirtsArray);
+};
+document.querySelector(".pant_btn").onclick = function btnclicked() {
+  chooseClothes(pantsArray);
+};
+document.querySelector(".skirt_btn").onclick = function btnclicked() {
+  chooseClothes(skirtArray);
+};
+document.querySelector(".blue_btn").onclick = function btnclicked() {
+  chooseClothes(blueArray);
+};
+document.querySelector(".pink_btn").onclick = function btnclicked() {
+  chooseClothes(pinkArray);
+};
+document.querySelector(".yellow_btn").onclick = function btnclicked() {
+  chooseClothes(yellowArray);
+}; **/
